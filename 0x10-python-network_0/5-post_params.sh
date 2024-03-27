1d2e5ec1b61aceb330d585ba4f17ee810c6006b2
Valentino40 <valentineodionye@gmail.com>
2024-03-27 10:23:03 -0700

    cURL to the end

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in a URL, sends a POST request to the passed URL, displays the body of the response
-curl "$1" -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+#Bash script that takes in a URL, sends a POST request to the passed URL
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
